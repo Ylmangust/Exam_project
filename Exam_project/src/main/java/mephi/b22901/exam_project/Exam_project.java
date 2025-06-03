01e2617f5bc62b1b584826335e6f05890673729e
Ylmangust <adeli-2019@mail.ru>
2025-06-03 23:07:58 +0300

    1st commit
    Частично создан пользовательский интерфейс.
    Необходимо реализовать:
    1. окно с диаграммой ганта
    2. окно со статистикой по участниками
    3. окно с информацией о задачах (история изменений, комментарии + вложения, прогресс)

---
+++ Exam_project/src/main/java/mephi/b22901/exam_project/Exam_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.exam_project;
+
+/**
+ *
+ * @author Регина
+ */
+public class Exam_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
